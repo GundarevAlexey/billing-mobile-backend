12cefddb7c16d86897a5bd43aefcba881d2361e8
jaloliddin <jalols1995@mail.ru>
2021-11-04 10:14:27 +0500

    add payment log

--- src/main/java/wg/rest/mobile/services/upay/UpayService.java
+++ src/main/java/wg/rest/mobile/services/upay/UpayService.java
@@ -1,8 +1,12 @@
 package wg.rest.mobile.services.upay;
 
 import com.apus.st.*;
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import wg.rest.mobile.dao.MobileDAO_Impl;
 
 import java.util.Date;
 
@@ -12,8 +16,12 @@ public class UpayService {
     @Autowired
     private UpayClient client;
 
+    private static final Logger logger = LoggerFactory.getLogger(UpayService.class);
+
+    Gson gson = new Gson();
 
     public PrepaymentResponse prepaymentResponse(Prepayment data) {
+        logger.warn("request -- {}", gson.toJson(data));
         Prepayment.PrepaymentRequest request = data.getPrepaymentRequest();
 
         Prepayment prepayment  = new Prepayment();
@@ -33,17 +41,18 @@ public class UpayService {
         ObjectFactory obj = new ObjectFactory();
         PrepaymentResponse prepaymentResponse = client.prepayment(UpayConst.WSDL_URL, obj.createPrepayment(prepayment));
 
+        logger.warn("response -- {}", gson.toJson(prepaymentResponse));
         return prepaymentResponse;
 
     }
 
     public ConfirmPaymentResponse confirmPaymentResponse(ConfirmPayment confirmPayment) {
-
+        logger.warn("request -- {}", gson.toJson(confirmPayment));
         confirmPayment.getConfirmPaymentRequest().setStPimsApiPartnerKey(UpayConst.KEY);
 
         ObjectFactory obj = new ObjectFactory();
         ConfirmPaymentResponse confirmPaymentResponse = client.confirmPayment(UpayConst.WSDL_URL, obj.createConfirmPayment(confirmPayment));
-
+        logger.warn("response -- {}", gson.toJson(confirmPaymentResponse));
         return confirmPaymentResponse;
 
     }
